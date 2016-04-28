0034e888e371cb563f0df0e3b7823f06b19aea67
Peter La <peterla@peterla.com>
2016-04-27 21:19:19 -0400

    Converted our AJAX call to jQuery.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,21 +1,37 @@
 $(document).on('ready page:load', function() {
-  function display_search_results() {
-    // readyState === 4 means that the asynchronous request completed successfully
-    if (this.readyState === 4) {
-      console.log(this);
-      document.getElementById('products').innerHTML = this.responseText;
-    }
-  }
+  // function display_search_results() {
+  //   // readyState === 4 means that the asynchronous request completed successfully
+  //   if (this.readyState === 4) {
+  //     console.log(this);
+  //     document.getElementById('products').innerHTML = this.responseText;
+  //   }
+  // }
+  //
+  // var form = document.getElementById('search-form');
+  // form.addEventListener('submit', function(event) {
+  //   event.preventDefault();
+  //   var searchValue = document.getElementById('search').value;
+  //
+  //   var xhr = new XMLHttpRequest();
+  //   xhr.onload = display_search_results;
+  //   xhr.open('GET', '/products?search=' + searchValue, true);
+  //   xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  //   xhr.send();
+  // });
 
-  var form = document.getElementById('search-form');
-  form.addEventListener('submit', function(event) {
+  $('#search-form').submit(function(event) {
     event.preventDefault();
-    var searchValue = document.getElementById('search').value;
+    var searchValue = $('#search').val();
 
-    var xhr = new XMLHttpRequest();
-    xhr.onload = display_search_results;
-    xhr.open('GET', '/products?search=' + searchValue, true);
-    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-    xhr.send();
+    $.ajax({
+      url: '/products?search=' + searchValue,
+      type: 'GET',
+      dataType: 'html'
+    }).success(function(response) {
+      $('#products').html(response);
+    });
+
+    // Using .success instead of .done as .done will execute after success or failure, and will do so no matter if
+    // there is a success or failure.
   });
 });
